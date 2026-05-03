69f04434811e5979c588b3670c52ed4f3c1d6f90
progamer63 <geometry1224gd@gmail.com>
2026-05-02 17:02:12 -0700

    added splashes, added changelog widget, removed lastfm widget for now

---
+++ splash.js
@@ -0,0 +1,11 @@
+const splashes = ['brand new!', 'recently changed!', 'exciting phrase!', 
+    'new and exciting!', 'handmade!', 'javascript inside!', '400 lines of css!', 
+    'correct horse<br>battery staple!', 'DROP TABLE USERS;--', 'it was dns.', 'ipv6 who?', 
+    'might be sentient!']
+
+function newSplash() {
+    const splash = document.querySelector('#splash');
+    let int = Math.floor(Math.random() * splashes.length);
+    let choice = splashes[int];
+    splash.innerHTML = choice;
+}
