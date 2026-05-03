3d4932719b6fcbb970ec98fe2262b1abc762c697
progamer63 <geometry1224gd@gmail.com>
2026-05-03 14:13:24 -0700

    tiny splash adjustment

--- splash.js
+++ splash.js
@@ -1,6 +1,6 @@
 const splashes = ['brand new!', 'recently changed!', 'exciting phrase!', 
     'new and exciting!', 'handmade!', 'javascript inside!', '400 lines of css!', 
-    'correct horse<br>battery staple!', 'DROP TABLE USERS;--', 'it was dns.', 'ipv6 who?', 
+    'correct horse battery staple!', 'DROP TABLE USERS;--', 'it was dns.', 'ipv6 who?', 
     'might be sentient!', 'made of sand!', 'hamster powered!']
 
 function newSplash() {
